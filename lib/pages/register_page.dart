8ccf5fffa794f53a7855c39f643e2fa32f309ea5
gule-fadil <gulekhalil453@gmail.com>
2022-06-16 20:34:41 +0300

    creating rounded image page

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -20,12 +20,40 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage>{
+  late double _deviceHeight;
+  late double _deviceWidth;
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+
+    return _buildUI();
   }
   Widget _buildUI(){
-    return Scaffold();
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: _deviceWidth * 0.03,
+          vertical: _deviceHeight * 0.02,
+        ),
+        height: _deviceHeight * 0.98,
+        width: _deviceWidth * 0.97,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _profileImageField(),
+            SizedBox(
+              height: _deviceHeight * 0.05,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  Widget _profileImageField(){
+    return
   }
 }
---
+++ lib/widgets/rounded_image.dart
@@ -0,0 +1,31 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+
+class RoundedImageNetwork extends StatelessWidget {
+  final String imagePath;
+  final double size;
+
+  RoundedImageNetwork({
+    required Key key,
+    required this.imagePath,
+    required this.size,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: size,
+      width: size,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: NetworkImage(imagePath),
+        ),
+        borderRadius: BorderRadius.all(
+          Radius.circular(size),
+        ),
+        color: Colors.black,
+      ),
+    );
+  }
+}
